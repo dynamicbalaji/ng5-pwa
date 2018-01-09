364fb7b6b0152732b4e9b6dbd9606ad0a503f710
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2018-01-08 21:48:52 -0600

    Added new component img-card with initial image

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ImgCardComponent } from './img-card/img-card.component';
+
+const routes: Routes = [
+  { path: '', component: ImgCardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { MatToolbarModule } from '@angular/material';
+import { MatToolbarModule, MatCardModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
+import { ImgCardComponent } from './img-card/img-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatToolbarModule,
+    MatToolbarModule, MatCardModule, 
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
