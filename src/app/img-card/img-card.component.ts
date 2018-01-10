61628df23b65bd3b7494352c6d71b5a573008c39
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2018-01-10 07:12:16 -0600

    Added offline support

--- src/app/img-card/img-card.component.ts
+++ src/app/img-card/img-card.component.ts
@@ -6,6 +6,12 @@ class CatImage {
   fontsize: number;
 }
 
+class Button { 
+  text: string;
+  disabled: boolean;
+  color: string;
+}
+
 @Component({
   selector: 'app-img-card',
   templateUrl: './img-card.component.html',
@@ -19,16 +25,25 @@ export class ImgCardComponent implements OnInit {
     fontsize: 40
   };
   public src: string;
+  public button: Button = {
+    text: 'Give me another cat',
+    color: 'primary',
+    disabled: false
+  };
   
   constructor() { }
   
   ngOnInit() {
-    this.generateSrc();
+    this.src = this.image.api + this.image.message + '?size=' + this.image.fontsize;
+    
+    if (!navigator.onLine) {
+      this.button.text = 'Sorry, you\'re offline';
+      this.button.disabled = true;
+    }
   }
 
   generateSrc(): void {
-    this.src = this.image.api + this.image.message +
-      '?size=' + this.image.fontsize + '&ts=' + Date.now();
+    this.src = this.src.replace(/\&ts=[\w]*/gi, '') + '&ts=' + Date.now();
   }
 
 }
